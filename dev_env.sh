75903599d9def5181d061efbafaacbe00dc12d0d
ifalo-ken <ken@ifalo.com>
2020-10-26 15:54:15 +0800

    Create dev_env.sh
    初始化 env設定值

---
+++ dev_env.sh
@@ -0,0 +1,9 @@
+export DB_HOST='localhost'
+export DB_USERNAME='demo_user'
+export DB_NAME='demo_db'
+export DB_PASSWORD='user_password'
+export DB_PORT=5432
+export DB_MAX_IDLE_CONN=10
+export DB_MAX_OPEN_CONN=20
+
+go run main.go
