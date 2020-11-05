7fed333a94c8dfb882ebb45b0889ec7933f81c2b
ifalo-ken <ken@ifalo.com>
2020-11-05 17:47:07 +0800

    add jwt token  rsa_key & lifetime

--- dev_env.sh
+++ dev_env.sh
@@ -6,4 +6,9 @@ export DB_PORT=5432
 export DB_MAX_IDLE_CONN=10
 export DB_MAX_OPEN_CONN=20
 
+export JWT_RSA_KEY_LOCATION='/opt/demo/private_key.pem'
+export JWT_OLD_RSA_KEY_LOCATION=''
+
+export JWT_TOKEN_LIFETIME=120
+
 go run main.go
